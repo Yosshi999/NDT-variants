09196daadd407a8864f1e90720b802e26080f35d
Yosshi999 <Yosshi999@users.noreply.github.com>
2021-02-13 19:00:55 +0900

    set same clang-format as PCL-1.11.1

--- include/ndt_variants/d2d_ndt.h
+++ include/ndt_variants/d2d_ndt.h
@@ -1,262 +1,269 @@
 #pragma once
 
-#include <pcl/pcl_macros.h>
 #include <pcl/common/utils.h>
-#include <pcl/registration/registration.h>
 #include <pcl/filters/voxel_grid_covariance.h>
+#include <pcl/registration/registration.h>
+#include <pcl/pcl_macros.h>
+
 #include <Eigen/StdVector>
 
-namespace pclex
-{
-  using namespace pcl;
-  using boost::shared_ptr;
-  using boost::make_shared;
+namespace pclex {
+using namespace pcl;
+using boost::shared_ptr;
+using boost::make_shared;
 
-  template<typename PointSource, typename PointTarget>
-  class D2DNDT : public Registration<PointSource, PointTarget>
+template <typename PointSource, typename PointTarget>
+class D2DNDT : public Registration<PointSource, PointTarget> {
+protected:
+  using Parent = Registration<PointSource, PointTarget>;
+
+  using PointCloudSource = typename Parent::PointCloudSource;
+
+  using PointCloudTarget = typename Parent::PointCloudTarget;
+  using PointCloudTargetConstPtr = typename PointCloudTarget::ConstPtr;
+
+  using SourceGrid = VoxelGridCovariance<PointSource>;
+  using SourceGridLeafConstPtr = typename SourceGrid::LeafConstPtr;
+  using TargetGrid = VoxelGridCovariance<PointTarget>;
+  using TargetGridLeafConstPtr = typename TargetGrid::LeafConstPtr;
+
+public:
+  using Ptr = shared_ptr<D2DNDT<PointSource, PointTarget>>;
+  using ConstPtr = shared_ptr<const D2DNDT<PointSource, PointTarget>>;
+
+  D2DNDT(float resolution = 1.0f, double step_size = 0.1, double outlier_ratio = 0.55);
+  ~D2DNDT() {}
+
+  inline void
+  setInputTarget(const PointCloudTargetConstPtr& cloud) override
   {
-    protected:
-      using Parent = Registration<PointSource, PointTarget>;
+    Parent::setInputTarget(cloud);
+    init();
+  }
 
-      using PointCloudSource = typename Parent::PointCloudSource;
+  inline double
+  getTransformationProbability() const
+  {
+    return trans_probability_;
+  }
 
-      using PointCloudTarget = typename Parent::PointCloudTarget;
-      using PointCloudTargetConstPtr = typename PointCloudTarget::ConstPtr;
+  inline int
+  getFinalNumIteration() const
+  {
+    return nr_iterations_;
+  }
 
-      using SourceGrid = VoxelGridCovariance<PointSource>;
-      using SourceGridLeafConstPtr = typename SourceGrid::LeafConstPtr;
-      using TargetGrid = VoxelGridCovariance<PointTarget>;
-      using TargetGridLeafConstPtr = typename TargetGrid::LeafConstPtr;
+  static inline Eigen::Matrix3d
+  computeWedge(double x, double y, double z)
+  {
+    Eigen::Matrix3d wedge;
+    wedge << 0, -z, y, z, 0, -x, -y, x, 0;
+    return wedge;
+  }
 
-    public:
-      using Ptr = shared_ptr< D2DNDT<PointSource, PointTarget> >;
-      using ConstPtr = shared_ptr< const D2DNDT<PointSource, PointTarget> >;
+  static inline Eigen::Matrix3d
+  computeExpWedge(double x, double y, double z)
+  {
+    Eigen::Vector3d v(x, y, z);
+    double norm = v.norm();
+    v.normalize();
+    Eigen::Matrix3d wedge = computeWedge(v(0), v(1), v(2));
+    auto mat = cos(norm) * Eigen::Matrix3d::Identity() +
+               (1 - cos(norm)) * v * v.transpose() + sin(norm) * wedge;
+    return mat;
+  }
 
-      D2DNDT(float resolution = 1.0f, double step_size = 0.1, double outlier_ratio = 0.55);
-      ~D2DNDT() {}
+  static void
+  convertTransform(const Eigen::Matrix<double, 6, 1>& x, Eigen::Affine3f& trans)
+  {
+    trans = Eigen::Translation<float, 3>(float(x(0)), float(x(1)), float(x(2))) *
+            computeExpWedge(x(3), x(4), x(5)).cast<float>();
+  }
 
-      inline void
-      setInputTarget (const PointCloudTargetConstPtr &cloud) override
-      {
-        Parent::setInputTarget(cloud);
-        init();
-      }
+  static void
+  convertTransform(const Eigen::Matrix<double, 6, 1>& x, Eigen::Matrix4f& trans)
+  {
+    Eigen::Affine3f _affine;
+    convertTransform(x, _affine);
+    trans = _affine.matrix();
+  }
 
-      inline double
-      getTransformationProbability () const
-      {
-        return trans_probability_;
-      }
+  static void
+  updateTransform(const Eigen::Matrix<double, 6, 1>& x, Eigen::Matrix4f& trans)
+  {
+    Eigen::Matrix4f delta;
+    convertTransform(x, delta);
+    trans = delta * trans;
+  }
 
-      inline int
-      getFinalNumIteration () const
-      {
-        return nr_iterations_;
-      }
+  inline void
+  setMinPointPerVoxel(int min_points_per_voxel)
+  {
+    min_points_per_voxel_ = min_points_per_voxel;
+  }
 
-      static inline Eigen::Matrix3d
-      computeWedge (double x, double y, double z)
-      {
-        Eigen::Matrix3d wedge;
-        wedge <<
-          0, -z, y,
-          z, 0, -x,
-          -y, x, 0;
-        return wedge;
-      }
+  inline int
+  getMinPointPerVoxel()
+  {
+    return min_points_per_voxel_;
+  }
 
-      static inline Eigen::Matrix3d
-      computeExpWedge (double x, double y, double z)
-      {
-        Eigen::Vector3d v(x,y,z);
-        double norm = v.norm();
-	      v.normalize();
-        Eigen::Matrix3d wedge = computeWedge(v(0), v(1), v(2));
-        auto mat = cos(norm) * Eigen::Matrix3d::Identity() +
-                   (1 - cos(norm)) * v * v.transpose() +
-                   sin(norm) * wedge;
-        return mat;
-      }
+protected:
+  using Parent::converged_;
+  using Parent::corr_dist_threshold_;
+  using Parent::final_transformation_;
+  using Parent::getClassName;
+  using Parent::indices_;
+  using Parent::inlier_threshold_;
+  using Parent::input_;
+  using Parent::max_iterations_;
+  using Parent::nr_iterations_;
+  using Parent::previous_transformation_;
+  using Parent::reg_name_;
+  using Parent::target_;
+  using Parent::transformation_;
+  using Parent::transformation_epsilon_;
+  using Parent::transformation_rotation_epsilon_;
 
-      static void
-      convertTransform (const Eigen::Matrix<double, 6, 1> &x, Eigen::Affine3f &trans)
-      {
-        trans = Eigen::Translation<float, 3>(float (x (0)), float (x (1)), float (x (2))) *
-                computeExpWedge(x (3), x (4), x (5)).cast<float>();
-      }
+  using Parent::update_visualizer_;
 
-      static void
-      convertTransform (const Eigen::Matrix<double, 6, 1> &x, Eigen::Matrix4f &trans)
-      {
-        Eigen::Affine3f _affine;
-        convertTransform (x, _affine);
-        trans = _affine.matrix ();
-      }
+  void
+  computeTransformation(PointCloudSource& output,
+                        const Eigen::Matrix4f& guess) override;
 
-      static void
-      updateTransform (const Eigen::Matrix<double,6,1> &x, Eigen::Matrix4f &trans)
-      {
-        Eigen::Matrix4f delta;
-        convertTransform(x, delta);
-      	trans = delta * trans;
-      }
+  void inline init()
+  {
+    target_cells_.setLeafSize(resolution_, resolution_, resolution_);
+    target_cells_.setInputCloud(target_);
+    target_cells_.setMinPointPerVoxel(min_points_per_voxel_);
+    // Initiate voxel structure.
+    target_cells_.filter(true);
+  }
 
-      inline void
-      setMinPointPerVoxel (int min_points_per_voxel)
-      {
-        min_points_per_voxel_ = min_points_per_voxel;
-      }
+  void inline initSource()
+  {
+    SourceGrid source_cells;
+    source_cells.setLeafSize(resolution_, resolution_, resolution_);
+    source_cells.setInputCloud(input_);
+    source_cells.setMinPointPerVoxel(min_points_per_voxel_);
+    source_cells.filter(false);
 
-      inline int
-      getMinPointPerVoxel ()
-      {
-        return min_points_per_voxel_;
-      }
+    source_covs_.clear();
+    source_means_.clear();
+    trans_covs_.clear();
+    trans_means_.clear();
+    for (const auto& kv : source_cells.getLeaves()) {
+      const auto& value = kv.second;
+      if (value.nr_points < min_points_per_voxel_)
+        continue;
+      source_covs_.push_back(value.getCov());
+      trans_covs_.push_back(value.getCov());
+      source_means_.push_back(value.getMean());
+      trans_means_.push_back(value.getMean());
+    }
+  }
 
-    protected:
-      using Parent::reg_name_;
-      using Parent::getClassName;
-      using Parent::input_;
-      using Parent::indices_;
-      using Parent::target_;
-      using Parent::nr_iterations_;
-      using Parent::max_iterations_;
-      using Parent::previous_transformation_;
-      using Parent::final_transformation_;
-      using Parent::transformation_;
-      using Parent::transformation_epsilon_;
-      using Parent::transformation_rotation_epsilon_;
-      using Parent::converged_;
-      using Parent::corr_dist_threshold_;
-      using Parent::inlier_threshold_;
+  void inline updateSource(const Eigen::Matrix4f& mat)
+  {
+    // std::cout << mat << std::endl;
+    Eigen::Matrix3d rot = mat.block<3, 3>(0, 0).template cast<double>();
+    Eigen::Vector3d tl = mat.block<3, 1>(0, 3).template cast<double>();
+    for (size_t i = 0; i < source_covs_.size(); i++) {
+      trans_covs_.at(i) = rot.transpose() * source_covs_.at(i) * rot;
+      trans_means_.at(i) = rot * source_means_.at(i) + tl;
+    }
+  }
 
-      using Parent::update_visualizer_;
+  double
+  computeDerivatives(Eigen::Matrix<double, 6, 1>& score_gradient,
+                     Eigen::Matrix<double, 6, 6>& hessian,
+                     bool compute_hessian = true);
 
-      void
-      computeTransformation (PointCloudSource &output, const Eigen::Matrix4f &guess) override;
+  double
+  updateDerivatives(Eigen::Matrix<double, 6, 1>& score_gradient,
+                    Eigen::Matrix<double, 6, 6>& hessian,
+                    const Eigen::Vector3d& x_trans,
+                    const Eigen::Matrix3d& c_inv,
+                    bool compute_hessian = true) const;
 
-      void inline
-      init ()
-      {
-        target_cells_.setLeafSize (resolution_, resolution_, resolution_);
-        target_cells_.setInputCloud ( target_ );
-	target_cells_.setMinPointPerVoxel (min_points_per_voxel_);
-        // Initiate voxel structure.
-        target_cells_.filter (true);
-      }
+  void
+  computePointDerivatives(const Eigen::Vector3d& x, bool compute_hessian = true);
 
-      void inline
-      initSource ()
-      {
-        SourceGrid source_cells;
-        source_cells.setLeafSize (resolution_, resolution_, resolution_);
-        source_cells.setInputCloud ( input_ );
-	source_cells.setMinPointPerVoxel (min_points_per_voxel_);
-        source_cells.filter (false);
+  void
+  computeHessian(Eigen::Matrix<double, 6, 6>& hessian);
 
-        source_covs_.clear();
-        source_means_.clear();
-	trans_covs_.clear();
-	trans_means_.clear();
-        for (const auto& kv : source_cells.getLeaves())
-        {
-          const auto& value = kv.second;
-          if (value.nr_points < min_points_per_voxel_) continue;
-          source_covs_.push_back(value.getCov());
-	  trans_covs_.push_back(value.getCov());
-          source_means_.push_back(value.getMean());
-	  trans_means_.push_back(value.getMean());
-        }
-      }
+  void
+  updateHessian(Eigen::Matrix<double, 6, 6>& hessian,
+                const Eigen::Vector3d& x_trans,
+                const Eigen::Matrix3d& c_inv) const;
 
-      void inline
-      updateSource (const Eigen::Matrix4f& mat)
-      {
-        //std::cout << mat << std::endl;
-        Eigen::Matrix3d rot = mat.block<3, 3>(0, 0).template cast<double>();
-        Eigen::Vector3d tl = mat.block<3, 1>(0, 3).template cast<double>();
-        for (size_t i = 0; i < source_covs_.size(); i++)
-        {
-          trans_covs_.at(i) = rot.transpose() * source_covs_.at(i) * rot;
-          trans_means_.at(i) = rot * source_means_.at(i) + tl;
-        }
-      }
+  double
+  computeStepLengthMT(Eigen::Matrix<double, 6, 1>& step_dir,
+                      double step_init,
+                      double step_max,
+                      double step_min,
+                      double& score,
+                      Eigen::Matrix<double, 6, 1>& score_gradient,
+                      Eigen::Matrix<double, 6, 6>& hessian);
 
-      double
-      computeDerivatives (Eigen::Matrix<double, 6, 1> &score_gradient,
-                          Eigen::Matrix<double, 6, 6> &hessian,
-                          bool compute_hessian = true);
+  bool
+  updateIntervalMT(double& a_l,
+                   double& f_l,
+                   double& g_l,
+                   double& a_u,
+                   double& f_u,
+                   double& g_u,
+                   double a_t,
+                   double f_t,
+                   double g_t) const;
 
-      double
-      updateDerivatives (Eigen::Matrix<double, 6, 1> &score_gradient,
-                         Eigen::Matrix<double, 6, 6> &hessian,
-                         const Eigen::Vector3d &x_trans, const Eigen::Matrix3d &c_inv,
-                         bool compute_hessian = true) const;
+  double
+  trialValueSelectionMT(double a_l,
+                        double f_l,
+                        double g_l,
+                        double a_u,
+                        double f_u,
+                        double g_u,
+                        double a_t,
+                        double f_t,
+                        double g_t) const;
 
-      void
-      computePointDerivatives (const Eigen::Vector3d &x, bool compute_hessian = true);
+  inline double
+  auxilaryFunction_PsiMT(
+      double a, double f_a, double f_0, double g_0, double mu = 1.e-4) const
+  {
+    return f_a - f_0 - mu * g_0 * a;
+  }
 
-      void
-      computeHessian (Eigen::Matrix<double, 6, 6> &hessian);
+  inline double
+  auxilaryFunction_dPsiMT(double g_a, double g_0, double mu = 1.e-4) const
+  {
+    return g_a - mu * g_0;
+  }
 
-      void
-      updateHessian (Eigen::Matrix<double, 6, 6> &hessian,
-                     const Eigen::Vector3d &x_trans, const Eigen::Matrix3d &c_inv) const;
+  TargetGrid target_cells_;
 
-      double
-      computeStepLengthMT (Eigen::Matrix<double, 6, 1> &step_dir,
-                           double step_init,
-                           double step_max, double step_min,
-                           double &score,
-                           Eigen::Matrix<double, 6, 1> &score_gradient,
-                           Eigen::Matrix<double, 6, 6> &hessian);
+  float resolution_;
+  double step_size_;
+  double outlier_ratio_;
+  int min_points_per_voxel_;
 
-      bool
-      updateIntervalMT (double &a_l, double &f_l, double &g_l,
-                        double &a_u, double &f_u, double &g_u,
-                        double a_t, double f_t, double g_t) const;
+  double gauss_d1_, gauss_d2_;
 
-      
-      double
-      trialValueSelectionMT (double a_l, double f_l, double g_l,
-                             double a_u, double f_u, double g_u,
-                             double a_t, double f_t, double g_t) const;
+  double trans_probability_;
 
-      inline double
-      auxilaryFunction_PsiMT (double a, double f_a, double f_0, double g_0, double mu = 1.e-4) const
-      {
-        return f_a - f_0 - mu * g_0 * a;
-      }
+  Eigen::Matrix<double, 3, 6> point_jacobian_;
+  Eigen::Matrix<double, 18, 6> point_hessian_;
 
-      inline double
-      auxilaryFunction_dPsiMT (double g_a, double g_0, double mu = 1.e-4) const
-      {
-        return g_a - mu * g_0;
-      }
+  std::vector<Eigen::Matrix3d, Eigen::aligned_allocator<Eigen::Matrix3d>> source_covs_;
+  std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>> source_means_;
+  std::vector<Eigen::Matrix3d, Eigen::aligned_allocator<Eigen::Matrix3d>> trans_covs_;
+  std::vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>> trans_means_;
 
-      TargetGrid target_cells_;
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
 
-      float resolution_;
-      double step_size_;
-      double outlier_ratio_;
-      int min_points_per_voxel_;
-
-      double gauss_d1_, gauss_d2_;
-
-      double trans_probability_;
-
-      Eigen::Matrix<double, 3, 6> point_jacobian_;
-      Eigen::Matrix<double, 18, 6> point_hessian_;
-
-      std::vector< Eigen::Matrix3d, Eigen::aligned_allocator<Eigen::Matrix3d> > source_covs_;
-      std::vector< Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > source_means_;
-      std::vector< Eigen::Matrix3d, Eigen::aligned_allocator<Eigen::Matrix3d> > trans_covs_;
-      std::vector< Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > trans_means_;
-
-    public:
-      EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-  };
-
-}
+} // namespace pclex
 
 #include "impl/d2d_ndt.hpp"
--- voxel_grid_filter.cpp
+++ voxel_grid_filter.cpp
@@ -1,20 +1,21 @@
 // https://pcl.readthedocs.io/projects/tutorials/en/latest/voxel_grid.html#voxelgrid
 // https://pcl.readthedocs.io/projects/tutorials/en/latest/cloud_viewer.html#cloud-viewer
 
-#include <iostream>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/visualization/cloud_viewer.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 
 #include <cmath>
+#include <iostream>
 #define PI 3.14159265
 
 using PC = pcl::PointCloud<pcl::PointXYZ>;
 using PPC = pcl::PointCloud<pcl::PointXYZ>::Ptr;
 using CPPC = pcl::PointCloud<pcl::PointXYZ>::ConstPtr;
 
-int main (int argc, char** argv)
+int
+main(int argc, char** argv)
 {
   constexpr int circle_num = 50;
   PC cloud;
@@ -23,22 +24,20 @@ int main (int argc, char** argv)
   cloud.is_dense = false;
   cloud.points.resize(cloud.width * cloud.height);
 
-  for (size_t i = 0; i < circle_num; i++)
-  {
+  for (size_t i = 0; i < circle_num; i++) {
     float x0 = 2 + cos(2 * PI / circle_num * i);
     float y0 = sin(2 * PI / circle_num * i);
     float z0 = 0;
-    for (size_t j = 0; j < circle_num; j++)
-    {
-      float x = x0 * cos(2 * PI / circle_num * j) - z0 * sin(2*PI / circle_num * j);
+    for (size_t j = 0; j < circle_num; j++) {
+      float x = x0 * cos(2 * PI / circle_num * j) - z0 * sin(2 * PI / circle_num * j);
       float y = y0;
-      float z = x0 * sin(2 * PI / circle_num * j) + z0 * cos(2*PI / circle_num * j);
-      cloud.points.at(circle_num * i + j) = pcl::PointXYZ(x,y,z);
+      float z = x0 * sin(2 * PI / circle_num * j) + z0 * cos(2 * PI / circle_num * j);
+      cloud.points.at(circle_num * i + j) = pcl::PointXYZ(x, y, z);
     }
   }
 
   CPPC cloud_ptr(new PC(cloud));
-  PPC cloud_filtered (new PC);
+  PPC cloud_filtered(new PC);
 
   // Create the filtering object
   pcl::VoxelGrid<pcl::PointXYZ> sor;
@@ -48,7 +47,8 @@ int main (int argc, char** argv)
 
   pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
   viewer.showCloud(cloud_filtered);
-  while (!viewer.wasStopped());
+  while (!viewer.wasStopped())
+    ;
 
   return (0);
 }
